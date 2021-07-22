c4b95c52c460d51bdfd765e446291ad95e97e3e3
Ubaid <ubaida985@gmail.com>
2021-07-23 01:38:16 +0530

    Task 2

---
+++ HelloToastChallenge/app/src/main/java/com/example/hellotoastchallenge/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.hellotoastchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    TextView showCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showCount = (TextView)findViewById(R.id.showCount);
+    }
+
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toastMessage, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void incrementCount( View view ){
+        ++ count;
+        showCount.setText(Integer.toString(count));
+    }
+}
