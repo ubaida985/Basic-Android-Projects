8279a455c635023599011cc5512c2130e38740f7
Ubaid <ubaida985@gmail.com>
2022-08-03 04:43:51 +0530

    basic

--- PaymentApp/app/src/main/java/com/example/paymentapp/ClientConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.example.paymentapp;
-
-public class ClientConfiguration {
-
-    public static final String PAYPAL_CLIENT_ID = "AUKoAr814Q8_qaxB6aSbhcmDrPChkRaaU0im15Sx9Shz1_x1_1sDPSuVClQpKHGe6SDY6WIA_UspPx31";
-
-}
